b64c420fd7d611a0f5ffc8d03a9a29f490dce6cd
Sapna Choudhary <sapnachoudhary97491@gmail.com>
2023-01-07 10:40:52 +0100

    Updates

---
+++ precache-manifest.82e40429b42c432a09fbb7c9aa7c4494.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bda3f08ccb733666ed6c0fb7137fde74",
+    "url": "/sapnachoudhary06/Add-to-cart/index.html"
+  },
+  {
+    "revision": "1210ddae7774f9ad490a",
+    "url": "/sapnachoudhary06/Add-to-cart/static/css/4.4932a258.chunk.css"
+  },
+  {
+    "revision": "1143d5e295198bdc6363",
+    "url": "/sapnachoudhary06/Add-to-cart/static/css/main.9f901fa6.chunk.css"
+  },
+  {
+    "revision": "1210ddae7774f9ad490a",
+    "url": "/sapnachoudhary06/Add-to-cart/static/js/4.4bdfddc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sapnachoudhary06/Add-to-cart/static/js/4.4bdfddc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af2bf8b2d724d622424e",
+    "url": "/sapnachoudhary06/Add-to-cart/static/js/5.e8128d87.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/sapnachoudhary06/Add-to-cart/static/js/5.e8128d87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8995b29055cbfd7ea84",
+    "url": "/sapnachoudhary06/Add-to-cart/static/js/6.dac5678d.chunk.js"
+  },
+  {
+    "revision": "1143d5e295198bdc6363",
+    "url": "/sapnachoudhary06/Add-to-cart/static/js/main.bf0c547a.chunk.js"
+  },
+  {
+    "revision": "bfdd05c91d13f6e10b18",
+    "url": "/sapnachoudhary06/Add-to-cart/static/js/runtime-main.07a6869e.js"
+  },
+  {
+    "revision": "544f917f47c16c70f09c",
+    "url": "/sapnachoudhary06/Add-to-cart/static/js/stencil-h8k-logo_2-entry-js.b56f3050.chunk.js"
+  },
+  {
+    "revision": "0f68a2f880c5bf3b07cb",
+    "url": "/sapnachoudhary06/Add-to-cart/static/js/stencil-polyfills-css-shim.5f0e96a3.chunk.js"
+  }
+]);
